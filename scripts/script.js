8d8d9ccca8374df8404cf423d4b3af4086039d53
clevergrant <clevergrant@gmail.com>
2017-11-15 23:58:13 -0700

    found out that getJSON actually implements the Promise class... so that's neat

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,17 @@
 $(() => {
 	var promiseArr = [];
 
-	promiseArr[0] = new Promise((resolve, reject) => { $.get("../database/items.json", json => resolve).fail(err => reject); });
-	promiseArr[1] = new Promise((resolve, reject) => { $.get("../database/portraits.json", json => resolve).fail(err => reject); });
-	promiseArr[2] = new Promise((resolve, reject) => { $.get("../database/classes.json", json => resolve).fail(err => reject); });
-	promiseArr[3] = new Promise((resolve, reject) => { $.get("../database/base-slot.json", json => resolve).fail(err => reject); });
+	promiseArr[0] = $.getJSON("../database/items.json");
+	promiseArr[1] = $.getJSON("../database/portraits.json");
+	promiseArr[2] = $.getJSON("../database/classes.json");
+	promiseArr[3] = $.getJSON("../database/base-slot.json");
 
 	Promise.all(promiseArr).then(dataArr => {
+
 		var items = dataArr[0];
 		var portraits = dataArr[1];
 		var classes = dataArr[2];
 		var baseSlot = dataArr[3];
-
-		console.log(items.Nothing);
 
 	});
 
