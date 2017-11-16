a5093f21280480a8900518ef34e8100648aa6753
clevergrant <clevergrant@gmail.com>
2017-11-15 23:48:02 -0700

    here come lots of pushes :)

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
 $(() => {
 	var promiseArr = [];
 
-	promiseArr[0] = new Promise((resolve, reject) => $.get("../database/items.json", json => resolve).fail(err => reject));
-	promiseArr[1] = new Promise((resolve, reject) => $.get("../database/portraits.json", json => resolve).fail(err => reject));
-	promiseArr[2] = new Promise((resolve, reject) => $.get("../database/classes.json", json => resolve).fail(err => reject));
-	promiseArr[3] = new Promise((resolve, reject) => $.get("../database/base-slot.json", json => resolve).fail(err => reject));
+	promiseArr[0] = new Promise((resolve, reject) => { $.get("../database/items.json", json => resolve).fail(err => reject); });
+	promiseArr[1] = new Promise((resolve, reject) => { $.get("../database/portraits.json", json => resolve).fail(err => reject); });
+	promiseArr[2] = new Promise((resolve, reject) => { $.get("../database/classes.json", json => resolve).fail(err => reject); });
+	promiseArr[3] = new Promise((resolve, reject) => { $.get("../database/base-slot.json", json => resolve).fail(err => reject); });
 
 	Promise.all(promiseArr).then(dataArr => {
 		var items = dataArr[0];
