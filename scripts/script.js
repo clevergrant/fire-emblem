f2f578129dc476b5d3da83b5480c8f484836385b
Grant Perdue <clevergrant@gmail.com>
2017-11-20 12:48:42 -0700

    I think this will fix it

--- scripts/script.js
+++ scripts/script.js
@@ -269,8 +269,8 @@ function getCodes() {
 		{
 			codeString: "10001BE0 0007",
 			description: "[M]",
-			rawaddress: 7136,
-			address: 268442592,
+			rawaddress: 268442592,
+			address: 134224864,
 			value: 7,
 			size: 512,
 			type: 1,
